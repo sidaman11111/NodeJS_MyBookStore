58fc7811f0052ab1c42aed4cfe0a9ae97b3e747d
SidaMan <sidaman11111@gmail.com>
2020-07-19 19:00:44 +0700

    Add PUG Template

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// CONFIG PUG TEMPLATE
+app.set('view engine', 'pug');
+app.set('views', './views');
+// END OF CONFIG PUG TEMPLATE
+
+// ROUTE FOR INDEX PAGE
+app.get('/', function(requests,response){
+      response.render('index');
+});
+// END OF ROUTE FOR INDEX PAGE  
 
 app.listen(port, () => console.log(`Example app listening PORT ${port}`));
